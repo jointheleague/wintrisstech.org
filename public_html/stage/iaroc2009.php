2d287cfc5e2ca0dc07a0df66d55d7987f650a351
Aaron VonderHaar <gruen0aermel@gmail.com>
2009-01-22 14:59:00 -0800

    Added iaroc 2009 flyer to the iaroc page.

--- public_html/stage/iaroc2009.php
+++ public_html/stage/iaroc2009.php
@@ -1,7 +1,10 @@
 <?php include( "header.php" )?>
 
-<img src="images/contestlogo.jpg" />
+<div style="text-align: center">
+  <img src="images/iaroc2009.png" />
+</div>
 
+<hr/>
 <li>
     <a href="http://www.flickr.com/photos/7442834@N02/sets/72157605813005035/">Photos of 2008's robotics competition</a>
 </li>
