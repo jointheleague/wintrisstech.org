7c01cfefd8e0f020d0f7f50658c7602c13c7ab47
Aaron VonderHaar <gruen0aermel@gmail.com>
2009-04-10 14:14:35 -0700

    Add new youtube videos to the iARoC page.

--- htdocs/iaroc2009.php
+++ htdocs/iaroc2009.php
@@ -1,10 +1,18 @@
 <?php include( "header.php" )?>
 
+<div>
+<object width="640" height="385"><param name="movie" value="http://www.youtube.com/v/uW3k7h3dxww&hl=en&fs=1&rel=0"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/uW3k7h3dxww&hl=en&fs=1&rel=0" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="640" height="385"></embed></object>
+</div>
+
 <div style="text-align: center">
   <img src="images/iaroc2009.png" />
 </div>
 
 <hr/>
+<h3>Last year's competition</h3>
+
+<object width="640" height="385"><param name="movie" value="http://www.youtube.com/v/3m0VN2ObfKk&hl=en&fs=1&rel=0"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/3m0VN2ObfKk&hl=en&fs=1&rel=0" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="640" height="385"></embed></object>
+
 <li>
     <a href="http://www.flickr.com/photos/7442834@N02/sets/72157605813005035/">Photos of 2008's robotics competition</a>
 </li>
