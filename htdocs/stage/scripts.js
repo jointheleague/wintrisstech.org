78dd2b36d6a2eb8b46be58646ae72715b7146364
Aaron VonderHaar <gruen0aermel@gmail.com>
2009-04-23 15:43:48 -0700

    Cleanup: remove old staging folder.

--- htdocs/stage/scripts.js
+++
@@ -1,7 +0,0 @@
-function showHide(action) {
-  if (action=='hide') {
-    setTimeout("document.getElementById('donors').style.visibility='hidden'",250);
-    } else {
-      document.getElementById('donors').style.visibility='visible';
-    }
-}
